920d2fe386e0cf38637c989e437eec7a1d52838e
cur2 <cur@humuson.com>
2024-07-24 08:58:10 +0900

    feat: 에러 Response 추가

--- src/test/java/com/gmm/modules/stock/StockRepositoryTest.java
+++ src/test/java/com/gmm/modules/stock/StockRepositoryTest.java
@@ -1,11 +1,9 @@
 package com.gmm.modules.stock;
 
-import com.gmm.infra.model.Money;
+import com.gmm.domain.StockFixture;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,18 +15,7 @@ class StockRepositoryTest {
 
     @Test
     void findById() {
-        final String ticker = "O";
-        final int quantity = 2;
-        final Money orderPrice = Money.of(57.48);
-        final LocalDate orderDate = LocalDate.of(2024, 7, 23);
-
-        Stock stock = Stock.builder()
-                .ticker(ticker)
-                .quantity(quantity)
-                .orderPrice(orderPrice)
-                .orderDate(orderDate)
-                .build();
-
+        Stock stock = StockFixture.anStock().build();
         stockRepository.save(stock);
 
         Stock findStock = stockRepository.findById(stock.getId()).orElseThrow();
