bcca32878bc1af7c976a38aa54492a2a4825f06b
cur2 <cur@humuson.com>
2024-07-23 13:41:42 +0900

    first commit

---
+++ src/main/java/com/gmm/GmmApplication.java
@@ -0,0 +1,13 @@
+package com.gmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmmApplication.class, args);
+	}
+
+}
